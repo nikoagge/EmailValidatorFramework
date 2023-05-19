b1a9354a9895157b5d3d2513cb2335a9fffc739c
nikoagge <nikoagge@gmail.com>
2023-05-19 14:35:12 +0300

    Added Xcode project

---
+++ EmailValidatorFramework/EmailValidatorFramework/EmailValidatorFramework.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidatorFramework.h
+//  EmailValidatorFramework
+//
+//  Created by Nikos Angelidis on 19/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidatorFramework.
+FOUNDATION_EXPORT double EmailValidatorFrameworkVersionNumber;
+
+//! Project version string for EmailValidatorFramework.
+FOUNDATION_EXPORT const unsigned char EmailValidatorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidatorFramework/PublicHeader.h>
+
+
